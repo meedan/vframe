8aa1972942871a4427e179c5c5a7fd8f74dc27fa
adamhrv <adam@ahprojects.com>
2019-05-13 20:10:26 +0200

    renamed

--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-./wait-for-it/wait-for-it.sh mysql:3306 && ./wait-for-it/wait-for-it.sh redis:6379
-npm i && npm run buildDev
-cd check && python cli_flask.py run --host=0.0.0.0
