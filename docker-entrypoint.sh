884fa783c9b1711cb54bec8a1eada2cf5fd48c4e
infojunkie <karim.ratib@gmail.com>
2019-05-25 13:22:49 -0700

    Integrate into Check dev

---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Start Redis (not yet needed)
+#./wait-for-it/wait-for-it.sh mysql:3306 && ./wait-for-it/wait-for-it.sh redis:6379
+
+# Init conda env
+source activate vframe
+
+# Add these to Dockerfile after tested
+
+# build JS
+npm i && npm run buildDev
+
+# run Flask app
+cd check && python cli_flask.py run --host=0.0.0.0
+
